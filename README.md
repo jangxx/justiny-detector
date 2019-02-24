7b34cb6a4ed87f6092219579bac0ef068f26bd5c
jangxx <janscheiper95@gmail.com>
2019-02-24 23:59:46 +0100

    added rest of the functionality, added readme

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# justiny-detector
+# Justin Y. Detector
 
 Server software which searches YouTube comments for Justin Y. and stores the result for cached access.
+
+## Installation
+
+Clone the repository and run
+
+	npm install
+
+Afterwards create a file called `.env` where you need to define the following environment variables:
+
+- `NODE_ENV`  
+Set this to `"production"`.
+- `SQLITE_PATH`  
+Path where you want the sqlite database to be placed
+- `API_KEY`  
+API key from the [google API console](https://console.cloud.google.com)
+- `PORT`  
+Port to bind the webserver to
+
+- `ADDRESS`  
+[optional] Address to bind the webserver to (Default: `""`, which means all addresses)
+- `LOG`  
+[optional] Set the loglevel (See the [loglevel docs](https://www.npmjs.com/package/loglevel) for allowed values) (Default: `"error"`)
+- `RECHECK_DELAY`  
+[optional] Number of seconds after which a video is queried again if no comment could be found the last time. (Default: 604800)
+
+After you have set your environment variables install the `knex` cli:
+
+	npm i -g knex
+
+and run
+
+	knex migrate:latest
+
+to initialize the database.
+
+## Running
+
+If you have installed everything, simply run
+
+	node ./
+
+to start the server.
--- index.js
+++ index.js
@@ -1,17 +1,112 @@
 require('dotenv').config();
 
+const JUSTINY_CHANNEL = "UCiTfB-A55Vq2fB610vaWJVA";
+const RECHECK_DELAY = (process.env.RECHECK_DELAY !== undefined) ? process.env.RECHECK_DELAY : 604800; // 7 days is the default
+
 const knexConfig = require('./knexfile');
+
+const path = require('path');
 
 const express = require('express');
 const log = require('loglevel');
-const knex = require('knex')(knexConfig[process.env.NODE_ENV]);
+const { google } = require('googleapis');
+const knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"]);
+const moment = require('moment');
 
 if (process.env.LOG != undefined) {
 	log.setLevel(process.env.LOG);
 }
 
+const youtube = google.youtube("v3");
 const app = express();
+
+const videoidRE = /^[a-zA-Z0-9_-]{11}$/;
+
+app.get("/api/check-and-retrieve", function(req, res) {
+	let video_id = req.query.v;
+
+	if (video_id == undefined) {
+		return res.sendStatus(400);
+	}
+
+	if (!videoidRE.test(video_id)) {
+		return res.sendStatus(400);
+	}
+
+	// check database if we've already seen this video_id
+	knex.select("comment_id", "last_checked").from("sightings").where({ video_id }).then(rows => {
+		if (rows.length == 0) {
+			return checkVideo(video_id).then(comment_id => storeComment(video_id, comment_id));
+		} else {
+			let row = rows[0];
+
+			if (row.comment_id !== null) {
+				return row.comment_id;
+			} else {
+				// see when we have last checked this video and check again if the time is right
+				let last_checked = moment(row.last_checked, "YYYY-MM-DD HH:mm:ss");
+
+				if (moment().utc().diff(last_checked, "seconds") > RECHECK_DELAY) {
+					return checkVideo(video_id).then(comment_id => updateComment(video_id, comment_id));
+				} else {
+					return null;
+				}
+			}
+		}
+	}).then(comment_id => {
+		return res.json({
+			comment_id,
+			video_id
+		});
+	}).catch(err => {
+		log.error(err.message);
+		return res.sendStatus(500);
+	});
+});
+
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(process.env.PORT, process.env.ADDRESS, () => {
 	log.info(`Server is listening on ${process.env.ADDRESS}:${process.env.PORT}`);
 });
+
+function checkVideo(id) {
+	log.info(`Checking YouTube API for video ${id}`);
+
+	return youtube.commentThreads.list({
+		key: process.env.API_KEY,
+		part: "snippet",
+		videoId: id,
+		searchTerms: "Justin Y.",
+		maxResults: 100
+	}).then(resp => {
+		let comments = resp.data.items;
+
+		for(let comment of comments) {
+			// check if the comment author matches Justin Y.
+			let author = comment.snippet.topLevelComment.snippet.authorChannelId.value;
+
+			if (author == JUSTINY_CHANNEL) {
+				return comment.id;
+			}
+		}
+
+		return null;
+	}).catch(err => {
+		log.error("[YouTube]", err.message);
+		return null; // store the null result in db so we don't recheck the same invalid urls over and over again
+	})
+}
+
+function storeComment(video_id, comment_id) {
+	return knex("sightings").insert({ video_id, comment_id }).then(() => comment_id);
+}
+
+function updateComment(video_id, comment_id) {
+	return knex("sighting").where({ video_id }).update({
+		comment_id, 
+		last_checked: moment().utc().format("YYYY-MM-DD HH:mm:ss") 
+	}).then(() => comment_id);
+}
